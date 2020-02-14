f8d4b2e497f21da2027c0df1d92bbd523e7b832a
woodong94 <gks3284@naver.com>
2020-02-14 16:50:43 +0900

    아주아주공ㅈ

--- [WEB][NAIVER][무엇이든 찍어주세요 만물도감]/site/user_setting.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE `user_setting` (
-  `USERID` VARCHAR(20) NOT NULL DEFAULT 'admin',
-  `repo_color` varchar(45) DEFAULT '#ffffff'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- [web][킴스스피치][킴스스피치]/react/client.js
+++
@@ -1,7 +0,0 @@
-const ReactDom = require('react-dom');
-const React = require('react');
-const {hot} = require('react-hot-loader/root');
-import Main from './Main';
-import './client.css';
-const Hot = hot(Main);
-ReactDom.render(<Hot/>, document.querySelector('#root')); 
